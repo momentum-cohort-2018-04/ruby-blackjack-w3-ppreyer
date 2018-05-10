e497533a235355b322263fc675b65892accc16a1
Parker <parker.preyer1@gmail.com>
2018-05-10 17:23:04 -0400

    initial class structure mapped out

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,1 +1,39 @@
-puts "TODO Implement the game of blackjack."
+class Game
+  # turn property
+  # winner property
+  # start game
+  # take turn
+  # reset round
+  # compare hands method
+end
+
+class Deck
+  # generate 52 card deck method
+  # list of all cards object
+  # pick random card 
+  # deal card to player method
+  # remove card from deck method
+end
+
+class Cards
+  # suit property
+  # rank property
+end
+
+class Player
+  # hand object
+  # wallet property
+  # hand total property
+  # hit or stay method
+  # calculate total
+end
+
+class Dealer < Player
+  # hand object
+  # hand total property
+  # hit or stay method
+  # auto compare method
+end
+
+
+
