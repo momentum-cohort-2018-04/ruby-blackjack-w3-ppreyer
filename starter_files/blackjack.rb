617cd748c723b1d341312daf8d7ba7700edb53aa
Parker <parker.preyer1@gmail.com>
2018-05-11 14:50:39 -0400

    created game and hand classes | added method to deal cards

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,10 +1,10 @@
 class Game
-  # turn property
-  # winner property
-  # start game
-  # take turn
-  # reset round
-  # compare hands method
+  def initialize
+  end
+  def deal_hand(hand, card_one, card_two)
+    hand.push(card_one) 
+    hand.push(card_two)
+  end
 end
 
 class Card
@@ -14,12 +14,15 @@ class Card
   end
 end
 
+# card = Card.new("Heart", "Q")
+
 class Deck
   attr_accessor :cards
   def initialize
     @ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     @suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
     @cards = []
+    # @player_hand = []
     
     @suits.map do |suit| 
       @ranks.map do |rank|
@@ -34,22 +37,30 @@ class Deck
   def pick_random_card
     @cards.delete(@cards.sample)  
   end
+
 end
 
-class Player
-  # hand object
-  # wallet property
-  # hand total property
-  # hit or stay method
-  # calculate total
+class Hand
+  attr_accessor :hand
+  def initialize
+    @hand = []
+  end
 end
 
-class Dealer < Player
-  # hand object
-  # hand total property
-  # hit or stay method
-  # auto compare method
-end
+# class Player
+#   # hand object
+#   # wallet property
+#   # hand total property
+#   # hit or stay method
+#   # calculate total
+# end
+
+# class Dealer < Player
+#   # hand object
+#   # hand total property
+#   # hit or stay method
+#   # auto compare method
+# end
 
 
 
